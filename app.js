98bbe136b3efea4be53bede3cb8129417f9f5295
parse_pecq <parse.pecq@gmail.com>
2021-07-08 01:09:52 +0100

    Test Netlify

--- app.js
+++ app.js
@@ -19,3 +19,23 @@ const swiper = new Swiper(".swiper-container", {
     el: ".swiper-scrollbar",
   },
 });
+var Geolocation = navigator.geolocation;
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log("Your current position is:");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+//console.log(Geolocation.getCurrentPosition(success, error, options));
